67aad91ae4e4db08d2132023857d3a7249ea4039
Arnobrizwan <147672066+Arnobrizwan@users.noreply.github.com>
2025-05-18 12:07:53 +0800

    Commit

---
+++ dhl_leave_management/lib/config/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyA0xUdG5tIAl1QSgGzVXFcHGTkF6LqlYnQ',
+      authDomain: 'dhl-leave-management-sys-92a37.firebaseapp.com',
+      projectId: 'dhl-leave-management-sys-92a37',
+      storageBucket: 'dhl-leave-management-sys-92a37.firebasestorage.app',
+      messagingSenderId: '467273473155',
+      appId: '1:467273473155:web:823dc0065777858aa38fb6',
+    );
+  }
+}
